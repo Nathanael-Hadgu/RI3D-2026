78d15d0c54370da59b3ba74b9c041c871e283ccd
sstock2005 <144393153+sstock2005@users.noreply.github.com>
2026-01-10 16:45:06 -0600

    + Updated WPILib to 2026.1.1 + Added 2026 Vendor Libraries + Supressed warnings

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,55 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {
-  public static class OperatorConstants {
+import static edu.wpi.first.units.Units.Amps;
+
+import edu.wpi.first.units.measure.Current;
+
+public class Constants 
+{
     public static final int kDriverControllerPort = 0;
-  }
+
+    public static final String kCanBusName = "rio";
+
+    public static final class DrivetrainConstants
+    {
+        public static final int kLeftLeaderPort = 2;
+        public static final int kLeftFollowerPort = 3;
+        public static final int kRightLeaderPort = 9;
+        public static final int kRightFollowerPort = 4;
+        public static final double kStickDeadband = 0.1;
+    }
+
+    public static final class JointConstants 
+    {
+        // Motor & Can
+        public static final int kJointMotorPort = 0;
+        public static final int kJointMotorCanPort = 0;
+
+        // PID & Feed forward
+        public static final double kP = 0; // Proportional Gain
+        public static final double kI = 0; // Integral Gain
+        public static final double kD = 0; // Derivative Gain
+        public static final double kG = 0; // Gravity Feedforward/Feedback Gain
+        public static final double kS = 0; // Static Feedforward Gain
+        public static final double kA = 0; // Acceleration Feedforward Gain
+        public static final double kV = 0; // Velocity Feedforward Gain
+
+        // Motion magic targets
+        public static final double kMotionMagicAcceleration = 0; // Target acceleration Motion Magic based control modes are allowed to use
+        public static final double kMotionMagicCruiseVel = 0; // Maximum velocity Motion Magic based control modes are allowed to use
+
+        // Gear ratio accounting
+        public static final double kRotorToSensorRatio = 1 / 5; // Set to gear ratio, example is 5:1 reduction
+
+        // Tolerance as encoder rotations
+        public static final int kTolerance = 0;
+
+        // Limits in encoder ticks
+        public static final int kUpperLimit = 0;
+        public static final int kLowerLimit = 0;
+
+        // Current limits
+        public static final Current kJointMotorStatorCurrentLimit = Amps.of(0);
+        public static final Current kJointMotorSupplyCurrentLimit = Amps.of(0);
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,79 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import frc.robot.Constants;
+
+public class DrivetrainSubsystem extends SubsystemBase
+{
+    SparkMax leftLeader;
+    SparkMax leftFollower;
+    SparkMax rightLeader;
+    SparkMax rightFollower;
+
+    @SuppressWarnings("removal")
+    public DrivetrainSubsystem()
+    {
+        // Initialize the SPARKs
+        leftLeader = new SparkMax(Constants.DrivetrainConstants.kLeftLeaderPort, MotorType.kBrushless);
+        leftFollower = new SparkMax(Constants.DrivetrainConstants.kLeftFollowerPort, MotorType.kBrushless);
+        rightLeader = new SparkMax(Constants.DrivetrainConstants.kRightLeaderPort, MotorType.kBrushless);
+        rightFollower = new SparkMax(Constants.DrivetrainConstants.kLeftFollowerPort, MotorType.kBrushless);
+
+        // Create new SPARK MAX configuration objects.
+        SparkMaxConfig globalConfig = new SparkMaxConfig();
+        SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+        SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
+        SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
+
+        // Set default parameters and set left leader config
+        globalConfig
+            .smartCurrentLimit(50)
+            .idleMode(IdleMode.kBrake);
+        
+        // Apply global config and invert since it is on the opposite side
+        rightLeaderConfig
+            .apply(globalConfig)
+            .inverted(true);
+        
+        // Apply global config and set leader for follower mode
+        leftFollowerConfig
+            .apply(globalConfig)
+            .follow(leftLeader);
+
+        // Apply the global config and set the leader for follower mode
+        rightFollowerConfig
+            .apply(globalConfig)
+            .follow(rightLeader);
+
+
+        // Apply configuration
+        // kResetSafeParameters is used to get to a known state.
+        // kPersistParameters is used to ensure config
+        leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    @Override
+    public void periodic()
+    {
+        SmartDashboard.putNumber("Left Out", leftLeader.getAppliedOutput());
+        SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
+    }
+
+    public void driveArcade(double forward, double rotation)
+    {
+        leftLeader.set(forward + rotation);
+        rightLeader.set(forward - rotation);
+    }
+}
