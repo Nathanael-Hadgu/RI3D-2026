e1631fd48e7f2292bc18c06964d8480aa0949c4e
sstock2005 <144393153+sstock2005@users.noreply.github.com>
2026-01-10 18:28:17 -0600

    Update Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,42 +14,8 @@ public class Constants
     {
         public static final int kLeftLeaderPort = 2;
         public static final int kLeftFollowerPort = 3;
-        public static final int kRightLeaderPort = 9;
-        public static final int kRightFollowerPort = 4;
+        public static final int kRightLeaderPort = 4;
+        public static final int kRightFollowerPort = 5;
         public static final double kStickDeadband = 0.1;
-    }
-
-    public static final class JointConstants 
-    {
-        // Motor & Can
-        public static final int kJointMotorPort = 0;
-        public static final int kJointMotorCanPort = 0;
-
-        // PID & Feed forward
-        public static final double kP = 0; // Proportional Gain
-        public static final double kI = 0; // Integral Gain
-        public static final double kD = 0; // Derivative Gain
-        public static final double kG = 0; // Gravity Feedforward/Feedback Gain
-        public static final double kS = 0; // Static Feedforward Gain
-        public static final double kA = 0; // Acceleration Feedforward Gain
-        public static final double kV = 0; // Velocity Feedforward Gain
-
-        // Motion magic targets
-        public static final double kMotionMagicAcceleration = 0; // Target acceleration Motion Magic based control modes are allowed to use
-        public static final double kMotionMagicCruiseVel = 0; // Maximum velocity Motion Magic based control modes are allowed to use
-
-        // Gear ratio accounting
-        public static final double kRotorToSensorRatio = 1 / 5; // Set to gear ratio, example is 5:1 reduction
-
-        // Tolerance as encoder rotations
-        public static final int kTolerance = 0;
-
-        // Limits in encoder ticks
-        public static final int kUpperLimit = 0;
-        public static final int kLowerLimit = 0;
-
-        // Current limits
-        public static final Current kJointMotorStatorCurrentLimit = Amps.of(0);
-        public static final Current kJointMotorSupplyCurrentLimit = Amps.of(0);
     }
 }
