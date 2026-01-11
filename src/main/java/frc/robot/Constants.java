a7f265cd9d7451828aaa2de268e4ae0749c975f8
dylanrattai <dylan.rattai@ndus.edu>
2026-01-11 11:45:29 -0600

    Create climb subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,6 +11,21 @@ public class Constants {
         public static final double kStickDeadband = 0.1;
     }
 
+    public static final class ClimbConstants {
+        public static final int kClimbMotorPort = 11;
+
+        // Current limits
+        public static final int kClimbStatorCurrentLimit = 120;
+        public static final int kClimbSupplyCurrentLimit = 60;
+
+        // climb motor run speed
+        public static final double kClimbSpeed = .1;
+
+        // motor soft limits
+        public static final int kClimbUpperLimit = 0;
+        public static final int kClimbLowerLimit = 0;
+    }
+
     public static final class IntakeConstants {
         // Port Constants
         public static final int kIntakeRotationLeaderPort = 0;
